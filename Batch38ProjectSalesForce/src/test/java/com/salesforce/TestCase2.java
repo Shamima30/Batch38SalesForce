d223f8e6dea7cf801cbd59c9ce40b35a4d6feb6b
shamima30 <nasrin.shamima30@gmail.com>
2024-05-30 21:46:24 -0400

    2nd test

---
+++ Batch38ProjectSalesForce/src/test/java/com/salesforce/TestCase2.java
@@ -0,0 +1,53 @@
+package com.salesforce;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+WebDriver driver;
+	
+	@Test
+	public  void tc_2() {
+		driver=new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		//validating the homepage of salesforce that salesforce word id there or not 
+		WebElement salesForceLogo = driver.findElement(By.id("logo"));
+	
+		if (salesForceLogo.isDisplayed()) {
+			System.out.println("Logo is Displayed");
+		
+			}
+			else {
+				System.out.println("Logo is not Displayed");
+			
+			}
+		//sending user name 
+		
+	driver.findElement(By.id("username")).sendKeys("nasrin.shamima30@gmail.com")	;
+	//sending password to the password field
+	WebElement passWordField = driver.findElement(By.id("password"));
+	passWordField.sendKeys("Bangladesh@3017");
+	//clicking on the Login Btn
+	driver.findElement(By.id("Login")).click();
+	// validating the user name is there 
+	   String  usrName = driver.findElement(By.id("userNavLabel")).getText();
+	   System.out.println(usrName);
+	   if (usrName.equals("Shamima Nasrin")) {
+		   System.out.println("user name is displayed then test case is passed");
+	   }
+	   else {
+		   System.out.println("user name is not displayed then test case is failed");
+	   }
+	
+
+
+	}}
+	
